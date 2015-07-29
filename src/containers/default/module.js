d24fc06e29ad29d1c9953ad2d7917f89b69ee2de
Tomas-Sereikis <tomas.sereikis@gmail.com>
2015-07-29 18:10:10 +0300

    removed some page

--- src/containers/default/module.js
+++ src/containers/default/module.js
@@ -3,19 +3,16 @@ import angular from "angular";
 import router from "angular-ui-router";
 import authorisation from "authorisation/module";
 import homePage from "./pages/home/module";
-import somePage from "./pages/some/module";
 import loginPage from "./pages/login/module";
 import artistPage from "./pages/artist/module";
 import songPage from "./pages/song/module";
 import defaultContainerConfig from "./config";
 import headerCtrl from "./controllers/headerCtrl";
 
-
 export default angular.module("msl.containers.default", [
   router,
   authorisation,
   homePage,
-  somePage,
   loginPage,
   artistPage,
   songPage
