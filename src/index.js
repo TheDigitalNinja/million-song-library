67d8faeeaa9e6b51fe705bb2d9694d3566ddd94e
Tomas-Sereikis <tomas.sereikis@gmail.com>
2015-07-30 21:13:15 +0300

    MSL-3 added user access controls

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import "bootstrap/less/bootstrap.less";
 import "font-awesome/less/font-awesome.less";
 import angular from "angular";
-import "angular-permission";
 import router from "angular-ui-router";
+import permission from "./modules/permission/module";
 import layouts from "./containers/module";
 import bootstrap from "./modules/bootstrap/module";
 import authorisation from "./modules/authorisation/module";
 import defaultConfig from "./config";
 import defaultRun from "./run";
 
-export default angular.module("msl", [router, "permission", layouts, bootstrap, authorisation])
+export default angular.module("msl", [router, permission, layouts, bootstrap, authorisation])
   .config(defaultConfig)
   .run(defaultRun)
   .name;
