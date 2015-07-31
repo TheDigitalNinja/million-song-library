af80ff22169dddf12aac031d77ea5cceace54c66
vgaldikas <vgaldikas@kenzanmedia.com>
2015-07-31 17:58:37 +0300

    MSL-29 Created Home page's HTMl mockup

--- src/containers/default/pages/home/module.js
+++ src/containers/default/pages/home/module.js
@@ -1,6 +1,8 @@
 import angular from "angular";
 import defaultContainerHomePageConfig from "./config";
+import homeCtrl from "./controllers/homeCtrl.js";
 
 export default angular.module("msl.containers.default.home", [])
   .config(defaultContainerHomePageConfig)
+  .controller("homeCtrl", homeCtrl)
   .name;
