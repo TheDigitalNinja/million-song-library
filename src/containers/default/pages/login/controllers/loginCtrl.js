97d05e4cac48eaa835e67e2bd18e6af462a73dfc
Tomas-Sereikis <tomas.sereikis@gmail.com>
2015-08-07 18:39:20 +0300

    MSL-34 authorisation error

--- src/containers/default/pages/login/controllers/loginCtrl.js
+++ src/containers/default/pages/login/controllers/loginCtrl.js
@@ -1,9 +1,15 @@
-function loginCtrl ($state, authorisation) {
+function loginCtrl ($scope, $state, authorisation) {
   "ngInject";
 
   this.submit = async () => {
-    await authorisation.authorise({login: this.email, password: this.password});
-    $state.go("default.home");
+    delete this.hasError;
+    try {
+      await authorisation.authorise({login: this.email, password: this.password});
+      $state.go("default.home");
+    } catch (e) {
+      this.hasError = true;
+      $scope.$evalAsync();
+    }
   };
 }
 
--- test/specs/containers/default/pages/login/controllers/loginCtrl.test.js
+++ test/specs/containers/default/pages/login/controllers/loginCtrl.test.js
@@ -22,10 +22,34 @@ describe("loginCtrl", function () {
 
   it("should authorise with credentials", done => async function () {
     var loginCtrl = $controller("loginCtrl", {$scope});
-    authorisation.authorise.and.returnValue(Promise.resolve(true));
+    authorisation.authorise.and.returnValue(Promise.resolve());
     loginCtrl.email = "test@test.com";
     loginCtrl.password = "password";
     await loginCtrl.submit();
+    expect(authorisation.authorise).toHaveBeenCalledWith({login: loginCtrl.email, password: loginCtrl.password});
+    expect($state.go).toHaveBeenCalledWith("default.home");
+    done();
+  }());
+
+  it("should fail to authorise and set hasError as true", done => async function () {
+    var loginCtrl = $controller("loginCtrl", {$scope});
+    authorisation.authorise.and.returnValue(Promise.reject());
+    await loginCtrl.submit();
+    expect(loginCtrl.hasError).toBe(true);
+    expect($state.go).not.toHaveBeenCalled();
+    done();
+  }());
+
+  it("should set hasError variable and then remove it", done => async function () {
+    var loginCtrl = $controller("loginCtrl", {$scope});
+    authorisation.authorise.and.returnValue(Promise.reject());
+    await loginCtrl.submit();
+    expect(loginCtrl.hasError).toBeTruthy();
+    authorisation.authorise.and.returnValue(Promise.resolve());
+    loginCtrl.email = "test@test.com";
+    loginCtrl.password = "password";
+    await loginCtrl.submit();
+    expect(loginCtrl.hasError).toBeUndefined();
     expect(authorisation.authorise).toHaveBeenCalledWith({login: loginCtrl.email, password: loginCtrl.password});
     expect($state.go).toHaveBeenCalledWith("default.home");
     done();
