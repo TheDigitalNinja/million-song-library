29a62609d176c585dfd5be5174213adc57bbaa59
Tomas-Sereikis <tomas.sereikis@gmail.com>
2015-08-13 18:11:52 +0300

    MSL-12 added history page

--- src/containers/default/pages/my/controllers/historyCtrl.js
+++ src/containers/default/pages/my/controllers/historyCtrl.js
@@ -1,5 +1,12 @@
-function historyCtrl () {
+function historyCtrl ($scope, recentSongsStore) {
   "ngInject";
+
+  // fetch content from my history store
+  (async () => {
+    this.content = await recentSongsStore.fetch();
+    this.display = true;
+    $scope.$evalAsync();
+  })();
 }
 
 export default historyCtrl;
--- src/modules/datastore/module.js
+++ src/modules/datastore/module.js
@@ -2,9 +2,11 @@ import angular from "angular";
 import request from "./request";
 import entityMapper from "./entityMapper";
 import myLibraryStore from "./services/myLibraryStore";
+import recentSongsStore from "./services/recentSongsStore";
 
 export default angular.module("msl.datastore", [])
   .factory("request", request)
   .factory("entityMapper", entityMapper)
   .factory("myLibraryStore", myLibraryStore)
+  .factory("recentSongsStore", recentSongsStore)
   .name;
