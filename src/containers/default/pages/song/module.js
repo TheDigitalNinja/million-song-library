5a6c7d125184104ba09dd47656c689616adf2075
vgaldikas <vgaldikas@kenzanmedia.com>
2015-08-13 17:05:18 +0300

    MSL-4 Getting song info from swagger

--- src/containers/default/pages/song/module.js
+++ src/containers/default/pages/song/module.js
@@ -1,10 +1,11 @@
 import "./stylesheets/default.less";
 import angular from "angular";
+import datastore from "modules/datastore/module";
 import defaultContainerSongPageConfig from "./config";
 import songCtrl from "./controllers/songCtrl.js";
 import rating from "modules/star-rating/module";
 
-export default angular.module("msl.containers.default.song", [rating])
+export default angular.module("msl.containers.default.song", [rating, datastore])
   .config(defaultContainerSongPageConfig)
   .controller("songCtrl", songCtrl)
   .name;
