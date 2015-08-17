ca199ae4f3a61f9a75882512d5f1b0a7d188ffe7
Tomas-Sereikis <tomas.sereikis@gmail.com>
2015-08-17 23:01:12 +0300

    added more documentation

--- src/modules/datastore/entityMapper.js
+++ src/modules/datastore/entityMapper.js
@@ -1,35 +1,51 @@
 import _ from "lodash";
 
 /**
- * @returns {entityMapper}
+ * response raw object mapper to typed entity
+ * @name entityMapper
+ * @type {Function}
  */
-function entityMapperFactory () {
+function entityMapper () {
   "ngInject";
 
   /**
-   * @name entityMapper
+   * @param {Object} response
+   * @param {Function} Entity
+   * @returns {*}
    */
-  function entityMapper (response, Entity) {
+  function mapper (response, Entity) {
+    // create new instance of entity
     var entity = new Entity();
-    var methods = _.keys(entity);
-    var Instance;
+    // get entity map keys
+    var keys = _.keys(entity);
+    // loop per response keys
     _.forEach(_.keys(response), function (key) {
+      // camel case response map key
       var method = _.camelCase(key);
+      // check if we can create instance of key type
       if (_.isFunction(entity[method])) {
-        Instance = entity[method];
+        // create new key type instance
+        var Instance = entity[method];
+        // assign it response value and get its instance
         entity[method] = new Instance(response[key]).valueOf();
-        methods = _.without(methods, method);
-      } else if (_.isArray(entity[method])) {
-        Instance = entity[method][0];
-        entity[method] = _.map(response[key], item => entityMapper(item, Instance));
-        methods = _.without(methods, method);
+        // remove key from needed to map keys list
+        keys = _.without(keys, method);
+      }
+      // check if map value is a array
+      else if (_.isArray(entity[method])) {
+        // map response items to type instance
+        entity[method] = _.map(response[key], item => mapper(item, entity[method][0]));
+        // remove key from needed to map keys list
+        keys = _.without(keys, method);
       }
     });
-    _.forEach(methods, method => delete entity[method]);
+    // remove keys that had not mapped to response keys
+    _.forEach(keys, method => delete entity[method]);
+
     return entity;
   }
 
-  return entityMapper;
+  return mapper;
 }
 
-export default entityMapperFactory;
+export default entityMapper;
