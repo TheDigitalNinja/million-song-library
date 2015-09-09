6ce6c5a863c2b603329e92a251abd0a28639c397
Brayan Z <brayan@pernix-solutions.com>
2015-09-09 17:03:56 -0600

    MSL-56: Error page routing test

---
+++ test/specs/pages/error/routes/error.route.test.js
@@ -0,0 +1,25 @@
+/* global describe, it, expect, beforeEach, inject, jasmine */
+
+import angular from 'angular';
+import errorPage from 'pages/error/error.module.js';
+
+describe('error route', () => {
+  var state;
+
+  beforeEach(() => {
+    angular.mock.module('ui.router');
+    angular.mock.module(errorPage);
+
+    inject(($state) => {
+      state = $state.get('error');
+    });
+  });
+
+  it('should use error template', () => {
+    expect(state.template).toEqual(require('pages/error/error.html'));
+  });
+
+  it('should have the error title', () => {
+    expect(state.title).toEqual('Error');
+  });
+});
