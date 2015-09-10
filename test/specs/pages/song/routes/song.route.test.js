f03cb7179226c0368f9510e5ae20394772352a1b
Brayan Z <brayan@pernix-solutions.com>
2015-09-10 11:49:44 -0600

    MSL-75: Create song routing tests

---
+++ test/specs/pages/song/routes/song.route.test.js
@@ -0,0 +1,40 @@
+/* global describe, it, expect, beforeEach, afterEach, inject, jasmine */
+
+import angular from 'angular';
+import layoutPage from 'layout/layout.module.js';
+import songPage from 'pages/song/song.module.js';
+
+describe('song routes', () => {
+  const songId = 2;
+  var state;
+  var $state;
+
+  beforeEach(() => {
+    angular.mock.module('ui.router');
+    angular.mock.module(layoutPage);
+    angular.mock.module(songPage);
+
+    inject((_$state_, $rootScope) => {
+      $state = _$state_;
+      $state.go('msl.song', { songId: songId });
+      $rootScope.$digest();
+      state = $state.current;
+    });
+  });
+
+  it('should include the songId on the url', () => {
+    expect($state.href(state)).toEqual(`#/song/${ songId }`);
+  });
+
+  it('should use the song template', () => {
+    expect(state.template).toEqual(require('pages/song/song.html'));
+  });
+
+  it('should use song controller', () => {
+    expect(state.controller).toEqual('songCtrl');
+  });
+
+  it('should use the controller as vm', () => {
+    expect(state.controllerAs).toEqual('vm');
+  });
+});
