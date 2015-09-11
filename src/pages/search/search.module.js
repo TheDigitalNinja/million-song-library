0d6c462427c094e3d1ae634dd7678e41b48ad6da
Jose Rodriguez <jrodriguez@pernix-solutions.com>
2015-09-10 20:47:17 -0600

    MSL-72: add search routing test

--- src/pages/search/search.module.js
+++ src/pages/search/search.module.js
@@ -8,7 +8,7 @@ import uiRouter from 'angular-ui-router';
 import searchPageRoute from './search.route.js';
 import searchCtrl from './controllers/search.controller.js';
 
-export default angular.module('msl.containers.default.search', [uiRouter])
+export default angular.module('msl.search', [uiRouter])
   .config(searchPageRoute)
   .controller('searchCtrl', searchCtrl)
   .name;
