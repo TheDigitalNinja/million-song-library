cf816af7f551e816acf06679202803633da88811
Brayan Z <brayan@pernix-solutions.com>
2015-10-05 13:30:34 -0600

    MSL-101: Create my library page

--- src/pages/library/controllers/library.controller.js
+++ src/pages/library/controllers/library.controller.js
@@ -1,10 +1,16 @@
+import _ from 'lodash';
+
 export default class libraryCtrl {
   constructor($scope, $log, myLibraryStore) {
     this.$scope = $scope;
     this.$log = $log;
     this.myLibraryStore = myLibraryStore;
 
+    this.songsPerSlide = 2;
+
     this.getLibrarySongs();
+    this.artistSlides = [];
+    this.albumSlides = [];
   }
 
   getLibrarySongs() {
@@ -12,6 +18,7 @@ export default class libraryCtrl {
       try {
         const response = await this.myLibraryStore.fetch();
         this.songs = response.songs;
+        this.songSlides = _.chunk(this.songs, this.songsPerSlide);
         this.$scope.$evalAsync();
       }
       catch(error) {
