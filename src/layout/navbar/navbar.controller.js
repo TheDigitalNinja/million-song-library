9d0e4b711233c1ddd59dd3e24f8203978139cd76
Brayan Z <brayan@pernix-solutions.com>
2015-10-20 10:09:42 -0600

    update navbar controller code and fix tests

--- src/layout/navbar/navbar.controller.js
+++ src/layout/navbar/navbar.controller.js
@@ -14,58 +14,57 @@ export default class navbarCtrl {
    * @param {msl.authorisation} authorisation
    */
   constructor($rootScope, $scope, $state, authorisation) {
+    this.$scope = $scope;
+    this.$state = $state;
+    this.authorisation = authorisation;
 
     /**
      * Check and see if we are on the home page so the template
      * knows whether to show the large or small hero.
      */
-
-    let vm = this;
-
-    vm.isHome = isHomeCheck($state.current.name);
+    this.isHome = this.isHomeCheck($state.current.name);
 
     $rootScope.$on('$stateChangeStart', (event, toState) => {
-      vm.isHome = isHomeCheck(toState.name);
+      this.isHome = this.isHomeCheck(toState.name);
     });
 
-    function isHomeCheck(newState) {
-      if (newState === 'msl.home') {
-        return true;
-      }
-      return false;
-    }
-
-    /**
-     * on authorisation state changes we what to catch that event
-     * and update controller authenticated user data
-     * vm event is not triggered by angular so we must manually
-     * trigger scope change
-     */
-
-    this.onStateChange = () => {
-      this.authorised = authorisation.isAuthorised();
-      if (this.authorised) {
-        this.email = authorisation.getUserData('userEmail');
-      }
-      else {
-        delete this.email;
-      }
-      $scope.$evalAsync();
-    };
-
-    /**
-     * logout action for destroying session
-     * after logout redirect user to login page
-     */
-    this.logout = async() => {
-      await authorisation.destroy();
-      $state.go('msl.login');
-    };
-
     // add authorisation state change listener
-    authorisation.addChangeListener(this.onStateChange);
+    this.authorisation.addChangeListener(this.onStateChange);
     // when scope destroys remove authorisation state change listener
     $scope.$on('$destroy', () => authorisation.removeChangeListener(this.onStateChange));
+  }
+
+  isHomeCheck(newState) {
+    if (newState === 'msl.home') {
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * on authorisation state changes we what to catch that event
+   * and update controller authenticated user data
+   * vm event is not triggered by angular so we must manually
+   * trigger scope change
+   */
+  onStateChange() {
+    this.authorised = this.authorisation.isAuthorised();
+    if (this.authorised) {
+      this.email = this.authorisation.getUserData('userEmail');
+    }
+    else {
+      delete this.email;
+    }
+    this.$scope.$evalAsync();
+  }
+
+  /**
+   * logout action for destroying session
+   * after logout redirect user to login page
+   */
+  async logout() {
+    await this.authorisation.destroy();
+    this.$state.go('msl.login');
   }
 
 }
