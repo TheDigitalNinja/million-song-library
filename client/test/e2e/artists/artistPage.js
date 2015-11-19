7c8c9f7dd9f406ffec24ef3ea7f0f800bbd9ebcf
Jose Rodriguez <jrodriguez@pernix-solutions.com>
2015-11-19 11:35:09 -0600

    fix protractor tests

--- client/test/e2e/artists/artistPage.js
+++ client/test/e2e/artists/artistPage.js
@@ -2,24 +2,23 @@ describe('artistPage', () => {
 
   beforeEach(() => {
     browser.driver.get(`${ browser.baseUrl }/artists/1`);
+    browser.driver.sleep(600);
   });
 
   it('checks artist name', () => {
-    const albumName = browser.driver.findElement(By.className('artist-name')).getText();
-    expect(albumName).toBe('La 12');
+    const artistName = browser.driver.findElement(By.css('[data-pt-id=artist-name]')).getText();
+    expect(artistName).toBe('La 12');
   });
 
   describe('nav in artist page', () => {
     describe('Songs', () => {
       it('checks song name', () => {
-        browser.driver.findElement(By.linkText('Songs')).click();
-        const songName = browser.driver.findElement(By.linkText('Liga Campeon')).getText();
+        const songName = browser.driver.findElement(By.css('songs-table td:nth-child(3) > a')).getText();
         expect(songName).toBe('Liga Campeon');
       });
 
       it('redirects to song page', () => {
-        browser.driver.findElement(By.linkText('Songs')).click();
-        browser.driver.findElement(By.linkText('Liga Campeon')).click();
+        browser.driver.findElement(By.css('songs-table td:nth-child(3) > a')).click();
         browser.getCurrentUrl().then((url) => {
           expect(url).toBe(`${ browser.baseUrl }/song/1`);
           debugger;
@@ -29,16 +28,19 @@ describe('artistPage', () => {
 
     describe('Albums', () => {
       it('checks album name', () => {
-        browser.driver.findElement(By.linkText('Albums')).click();
-        const albumName = browser.driver.findElement(By.linkText('Some album')).getText();
+        browser.driver.findElement(By.css('md-tab-item:nth-child(2) span')).click();
+        browser.driver.sleep(600);
+        const albumName = browser.driver.findElement(By.css('albums-list album-box a')).getText();
         expect(albumName).toBe('Some album');
       });
 
       it('redirects to album page', () => {
-        browser.driver.findElement(By.linkText('Albums')).click();
-        browser.driver.findElement(By.linkText('Some album')).click();
+        browser.driver.findElement(By.css('md-tab-item:nth-child(2) span')).click();
+        browser.driver.sleep(600);
+        browser.driver.findElement(By.css('albums-list album-box a')).click();
+        browser.driver.sleep(600);
         browser.getCurrentUrl().then((url) => {
-          expect(url).toBe('http://localhost:3000/#/album/1');
+          expect(url).toBe(`${ browser.baseUrl }/album/1`);
           debugger;
         });
       });
@@ -46,16 +48,18 @@ describe('artistPage', () => {
 
     describe('Similar Artists', () => {
       it('checks artist name', () => {
-        browser.driver.findElement(By.linkText('Similar artists')).click();
-        const albumName = browser.driver.findElement(By.linkText('Mercyful Fate')).getText();
+        browser.driver.findElement(By.css('md-tab-item:nth-child(3) span')).click();
+        browser.driver.sleep(600);
+        const albumName = browser.driver.findElement(By.css('artists-list artist-box div.media-box-meta a')).getText();
         expect(albumName).toBe('Mercyful Fate');
       });
 
       it('redirects to artist page', () => {
-        browser.driver.findElement(By.linkText('Similar artists')).click();
-        browser.driver.findElement(By.linkText('Mercyful Fate')).click();
+        browser.driver.findElement(By.css('md-tab-item:nth-child(3) span')).click();
+        browser.driver.sleep(600);
+        browser.driver.findElement(By.css('artists-list artist-box div.media-box-meta a')).click();
         browser.getCurrentUrl().then((url) => {
-          expect(url).toBe('http://localhost:3000/#/artists/2');
+          expect(url).toBe(`${ browser.baseUrl }/artists/2`);
           debugger;
         });
       });
--- client/test/e2e/songs/songPage.js
+++ client/test/e2e/songs/songPage.js
@@ -1,28 +1,71 @@
-describe('sonPage', () => {
+describe('songPage', () => {
 
   beforeEach(() => {
     browser.driver.get(`${ browser.baseUrl }/song/1`);
+    browser.driver.sleep(600);
   });
 
   describe('checks song info', () => {
     it('checks song name', () => {
-      const songName = browser.driver.findElement(By.className('song-name')).getText();
+      const songName = browser.driver.findElement(By.css('[data-pt-id=song-name]')).getText();
       expect(songName).toMatch('Liga Campeon');
     });
 
-    it('checks artist name', () => {
-      const artistName = browser.driver.findElement(By.linkText('La 12')).getText();
-      expect(artistName).toMatch('La 12');
+    describe('checks artist info', () => {
+      it('checks artist name', () => {
+        const artistName = browser.driver.findElement(By.css('[data-pt-id=song-artist-name]')).getText();
+        expect(artistName).toMatch('La 12');
+      });
+
+      it('redirects to artist page', () => {
+        browser.driver.findElement(By.css('[data-pt-id=song-artist-name]')).click();
+        browser.getCurrentUrl().then((url) => {
+          expect(url).toBe(`${ browser.baseUrl }/artists/1`);
+          debugger;
+        });
+      });
+    });
+
+    describe('checks album info', () => {
+      it('checks album name', () => {
+        const artistName = browser.driver.findElement(By.css('[data-pt-id=song-album-name]')).getText();
+        expect(artistName).toMatch('Some album');
+      });
+      it('redirects to album page', () => {
+        browser.driver.findElement(By.css('[data-pt-id=song-album-name]')).click();
+        browser.getCurrentUrl().then((url) => {
+          expect(url).toBe(`${ browser.baseUrl }/album/1`);
+          debugger;
+        });
+      });
+
     });
 
     it('checks genre name', () => {
-      const genreName = browser.driver.findElement(By.className('genre-name')).getText();
+      const genreName = browser.driver.findElement(By.css('[data-pt-id=song-genre]')).getText();
       expect(genreName).toMatch('Classic');
     });
 
     it('checks song release', () => {
-      const songRelease = browser.driver.findElement(By.className('song-release')).getText();
+      const songRelease = browser.driver.findElement(By.css('[data-pt-id=song-year]')).getText();
       expect(songRelease).toMatch('1919');
+    });
+  });
+
+  describe('checks similar artist', () => {
+    it('checks similar artist name', () => {
+      browser.driver.sleep(600);
+      const artistName = browser.driver.findElement(By.css('artists-list artist-box div.media-box-meta a')).getText();
+      expect(artistName).toMatch('Mercyful Fate');
+    });
+
+    it('redirects to artist page', () => {
+      browser.driver.sleep(600);
+      browser.driver.findElement(By.css('artists-list artist-box div.media-box-meta a')).click();
+      browser.getCurrentUrl().then((url) => {
+        expect(url).toBe(`${ browser.baseUrl }/artists/2`);
+        debugger;
+      });
     });
   });
 });
