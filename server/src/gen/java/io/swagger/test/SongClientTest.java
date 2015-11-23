d92ef12b4fd1fbad3f5c6691be2e042217ec42f8
anram88 <aramirez@kenzanmedia.com>
2015-11-23 09:17:08 -0600

    MSL-306: updated to jersey 2.2 for added testing capabilities

--- server/src/gen/java/io/swagger/test/SongClientTest.java
+++ server/src/gen/java/io/swagger/test/SongClientTest.java
@@ -1,14 +1,15 @@
 package io.swagger.tests;
 
+import io.swagger.api.impl.MslApiResponseMessage;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.Before;
 import io.swagger.client.SongClient;
-import javax.ws.rs.core.Response;
-import java.math.BigDecimal;
+import javax.management.RuntimeErrorException;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Created by anram88 on 11/16/15.
@@ -21,35 +22,50 @@ public class SongClientTest {
     @Before
     public void init () {
         songClient = new SongClient();
-        logger.setLevel(Level.DEBUG);
+        logger.setLevel(Level.INFO);
     }
 
     @Test
     public void testGet() {
         logger.debug("songClient.testGet");
-        Response song = songClient.get("1");
-        assertEquals("song get call is successful", 200, song.getStatus());
+        MslApiResponseMessage song = songClient.get("1");
+        assertNotNull(song);
+        assertEquals("song get call is successful", "success", song.getMessage());
     }
+
+    @Test (expected = java.lang.RuntimeException.class)
+    public void testGetExceptionIsThrown() {
+        logger.debug("songClient.testGetExceptionIsThrown");
+        songClient.get("");
+    }
+
 
     @Test
     public void testBrowse() {
         logger.debug("songClient.testBrowse");
-        Response songList = songClient.browse("");
-        assertEquals("song browse call is successful", 200, songList.getStatus());
+        MslApiResponseMessage songList = songClient.browse("");
+        assertEquals("song browse call is successful", "success", songList.getMessage());
     }
 
     @Test
     public void testBrowseFilteredByFacets() {
         logger.debug("songClient.testBrowseFilteredByFacets");
-        Response albumList = songClient.browse("3");
-        assertEquals("songs browse facet filtered call is successful", 200, albumList.getStatus());
+        MslApiResponseMessage albumList = songClient.browse("3");
+        assertEquals("songs browse facet filtered call is successful", "success", albumList.getMessage());
     }
 
     @Test
     public void testAddSong() {
         logger.debug("songClient.testAddSong");
-        Response response = songClient.addSong("1", "someToken");
-        assertEquals("addSong response is successful", 200, response.getStatus());
+        MslApiResponseMessage response = songClient.addSong("1", "someToken");
+        assertNotNull(response);
+        assertEquals("addSong response is successful", "magic!", response.getMessage());
+    }
+
+    @Test (expected=java.lang.RuntimeException.class)
+    public void testAddSongThrowException (){
+        logger.debug("songClient.testAddSongThrowException");
+        songClient.addSong("", "");
     }
 
 }
